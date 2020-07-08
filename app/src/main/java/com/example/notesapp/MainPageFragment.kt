43fd5631e54260a95bf3c9515002a86d8bf59d09
Ro4052 <Ro4052@users.noreply.github.com>
2020-07-08 14:25:24 +0100

    intial commit

---
+++ app/src/main/java/com/example/notesapp/MainPageFragment.kt
@@ -0,0 +1,29 @@
+package com.example.notesapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainPageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_page, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<FloatingActionButton>(R.id.new_note_fab).setOnClickListener {
+            Toast.makeText(context, getString(R.string.create_note_toast), Toast.LENGTH_SHORT).show()
+        }
+    }
+}
